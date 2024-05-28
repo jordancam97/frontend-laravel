354da1815e8476d1e560dd6432892402db2448b4
Jordancam97 <jordangw97@gmail.com>
2024-05-28 06:41:26 -0500

    applying first styles

--- src/components/CreateEmpleados.js
+++ src/components/CreateEmpleados.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const endpoint = "http://localhost:8000/api/empleado";
 
@@ -30,79 +31,122 @@ const CreateEmpleados = () => {
   };
 
   return (
-    <div className="px-4 ">
-      <h3 className="mt-5 mb-3">Agregar un nuevo empleado</h3>
-      <form onSubmit={store}>
-        <div className="row ">
-          <div className="mb-3 col-md-6">
-            <label className="form-label">Nombre</label>
-            <input
-              value={nombre}
-              onChange={(e) => setNombre(e.target.value)}
-              type="text"
-              className="form-control"
-            />
-          </div>
-          <div className="mb-3 col-md-6">
-            <label className="form-label">Apellido</label>
-            <input
-              value={apellido}
-              onChange={(e) => setApellido(e.target.value)}
-              type="text"
-              className="form-control"
-            />
-          </div>
-          <div className="mb-3 col-md-6">
-            <label className="form-label">Razón social</label>
-            <input
-              value={razon}
-              onChange={(e) => setRazon(e.target.value)}
-              type="text"
-              className="form-control"
-            />
-          </div>
-          <div className="mb-3 col-md-6">
-            <label className="form-label">Cédula</label>
-            <input
-              value={cedula}
-              onChange={(e) => setCedula(e.target.value)}
-              type="number"
-              className="form-control"
-            />
-          </div>
-          <div className="mb-3 col-md-6">
-            <label className="form-label">Teléfono</label>
-            <input
-              value={telefono}
-              onChange={(e) => setTelefono(e.target.value)}
-              type="number"
-              className="form-control"
-            />
-          </div>
-          <div className="mb-3 col-md-6">
-            <label className="form-label">País</label>
-            <input
-              value={pais}
-              onChange={(e) => setPais(e.target.value)}
-              type="text"
-              className="form-control"
-            />
-          </div>
-          <div className="mb-3 col-md-6">
-            <label className="form-label">Ciudad</label>
-            <input
-              value={ciudad}
-              onChange={(e) => setCiudad(e.target.value)}
-              type="text"
-              className="form-control"
-            />
+    <div
+      className="px-4 d-flex justify-content-center align-items-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div class="card m-2">
+        <h5 class="card-header">Crud React + Laravel</h5>
+        <div class="card-body">
+          <h6 class="card-title">Agregar un nuevo empleado</h6>
+          <div className="text-start mt-3">
+            <form onSubmit={store}>
+              <div className="row">
+                <div className="col-md-6 mb-3">
+                  <label htmlFor="nombre" className="form-label">
+                    Nombre
+                  </label>
+                  <input
+                    id="nombre"
+                    value={nombre}
+                    onChange={(e) => setNombre(e.target.value)}
+                    type="text"
+                    className="form-control"
+                    required
+                  />
+                </div>
+                <div className="col-md-6 mb-3">
+                  <label htmlFor="apellido" className="form-label">
+                    Apellido
+                  </label>
+                  <input
+                    id="apellido"
+                    value={apellido}
+                    onChange={(e) => setApellido(e.target.value)}
+                    type="text"
+                    className="form-control"
+                    required
+                  />
+                </div>
+                <div className="col-md-6 mb-3">
+                  <label htmlFor="razon" className="form-label">
+                    Razón social
+                  </label>
+                  <input
+                    id="razon"
+                    value={razon}
+                    onChange={(e) => setRazon(e.target.value)}
+                    type="text"
+                    className="form-control"
+                    required
+                  />
+                </div>
+                <div className="col-md-6 mb-3">
+                  <label htmlFor="cedula" className="form-label">
+                    Cédula
+                  </label>
+                  <input
+                    id="cedula"
+                    value={cedula}
+                    onChange={(e) => setCedula(e.target.value)}
+                    type="number"
+                    className="form-control"
+                    required
+                  />
+                </div>
+                <div className="col-md-6 mb-3">
+                  <label htmlFor="telefono" className="form-label">
+                    Teléfono
+                  </label>
+                  <input
+                    id="telefono"
+                    value={telefono}
+                    onChange={(e) => setTelefono(e.target.value)}
+                    type="number"
+                    className="form-control"
+                    required
+                  />
+                </div>
+                <div className="col-md-6 mb-3">
+                  <label htmlFor="pais" className="form-label">
+                    País
+                  </label>
+                  <input
+                    id="pais"
+                    value={pais}
+                    onChange={(e) => setPais(e.target.value)}
+                    type="text"
+                    className="form-control"
+                    required
+                  />
+                </div>
+                <div className="col-md-6 mb-3">
+                  <label htmlFor="ciudad" className="form-label">
+                    Ciudad
+                  </label>
+                  <input
+                    id="ciudad"
+                    value={ciudad}
+                    onChange={(e) => setCiudad(e.target.value)}
+                    type="text"
+                    className="form-control"
+                    required
+                  />
+                </div>
+              </div>
+
+              <div className="d-grid gap-2 d-md-flex justify-content-md-end">
+                <Link to="/" className="btn btn-secondary me-md-2 mb-2 mb-md-0">
+                  Regresar
+                </Link>
+                <button type="submit" className="btn btn-primary">
+                  Guardar
+                </button>
+              </div>
+            </form>
           </div>
         </div>
-
-        <button type="submit" className="btn btn-primary">
-          Guardar
-        </button>
-      </form>
+      </div>
     </div>
   );
 };
